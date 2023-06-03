4d46be218994269b738c0b167f715281f381c7ea
Dennis JB Rocha <58828866+dennis-rocha@users.noreply.github.com>
2023-06-03 16:45:51 -0300

    Create exercicios.py

---
+++ modulo-1/aula-3/exercicios.py
@@ -0,0 +1,39 @@
+# Atividade 1:
+# Escreva um programa que verifique se um número é positivo. 
+# Se for positivo, imprima "O número é positivo".
+
+
+# Atividade 2:
+# Escreva um programa que verifique se um número é par. 
+# Se for par, imprima "O número é par".
+
+
+# Atividade 3:
+# Escreva um programa que verifique se uma pessoa é maior ou menor de idade. 
+# Se for maior de idade, imprima "Você é maior de idade". 
+# Caso contrário, imprima "Você é menor de idade".
+
+
+# Atividade 4:
+# Escreva um programa que verifique se um número é positivo ou 
+# negativo. Imprima a respectiva mensagem de acordo com a condição.
+
+
+# Atividade 5:
+# Escreva um programa que verifique se um número é positivo, negativo ou zero. 
+# Imprima a respectiva mensagem "O número não é zero e é positivo", 
+# "O número não é zero e é negativo" ou "O número é zero.
+
+
+# Atividade 6:
+# Escreva um programa que verifique se um número é divisível por 2 e por 3 ao mesmo tempo. 
+# Se for, imprima "O número é divisível por 2 e por 3".
+
+# Atividade 7:
+# Escreva um programa que imprima o sálario baseado no cargo do funcionário. 
+# Os cargos e os sálarios são:
+"""
+Desenvolvedor de Sistemas: R$ 4500
+Gerente de Desenvolvimento: R$ 9100
+Cientista de Dados: R$ 6350
+"""
